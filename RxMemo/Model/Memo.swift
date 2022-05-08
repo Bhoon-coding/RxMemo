dc7f0a19fed771f3288af7a1dae95b19cdf4865d
bhoon-coding <bhooncoding@gmail.com>
2022-05-08 23:28:14 +0900

    메모 Model 추가

---
+++ RxMemo/Model/Memo.swift
@@ -0,0 +1,25 @@
+//
+//  Memo.swift
+//  RxMemo
+//
+//  Created by BH on 2022/05/08.
+//
+
+import Foundation
+
+struct Memo: Equatable {
+    var content: String
+    var insertDate: Date
+    var identity: String
+    
+    init(content: String, insertDate: Date = Date()) {
+        self.content = content
+        self.insertDate = insertDate
+        self.identity = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    init(original: Memo, updatedContent: String) {
+        self = original
+        self.content = updatedContent
+    }
+}
